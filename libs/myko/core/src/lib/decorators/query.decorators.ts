a13e45de30bafdafcc0edccad1ac70df26e7bade
Trevor Sargent <trevor.a.sargent@gmail.com>
2023-03-10 17:25:40 -0800

    fix(myko): queries have queryId not commandId

--- libs/myko/core/src/lib/decorators/query.decorators.ts
+++ libs/myko/core/src/lib/decorators/query.decorators.ts
@@ -10,17 +10,17 @@ import {
 import {} from '../types'
 
 export const MykoQuery =
-  <U extends MItem>(commandId: string, item: new (...args: any[]) => U) =>
+  <U extends MItem>(queryId: string, item: new (...args: any[]) => U) =>
   <T extends MQuery>(target: new (...args: any[]) => T) => {
     const itemType = Reflect.getMetadata(MYKO_ITEM_TYPE, item)
     const original: any = target
     const withType: any = function (...args: any[]) {
       const typed = new original(...args)
-      Reflect.defineMetadata(MYKO_QUERY_ID_KEY, commandId, typed)
+      Reflect.defineMetadata(MYKO_QUERY_ID_KEY, queryId, typed)
       Reflect.defineMetadata(MYKO_QUERY_ITEM_TYPE_KEY, itemType, typed)
       return typed
     }
-    Reflect.defineMetadata(MYKO_QUERY_ID_KEY, commandId, withType)
+    Reflect.defineMetadata(MYKO_QUERY_ID_KEY, queryId, withType)
     Reflect.defineMetadata(MYKO_QUERY_ITEM_TYPE_KEY, itemType, withType)
     return withType
   }
