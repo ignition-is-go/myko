20851e8da0bc9cef901c08e8a7fc6ea08b7226ce
Trevor Sargent <trevor.a.sargent@gmail.com>
2023-05-05 12:54:13 -0700

    feat(myko): use md5 hash

--- libs/myko/core/src/lib/types/item.ts
+++ libs/myko/core/src/lib/types/item.ts
@@ -1,6 +1,7 @@
 import type { ID, PartialBy } from './base'
-
 export const MYKO_ITEM_TYPE = '__MYKO_ITEM_TYPE__'
+
+import { MD5 } from 'object-hash'
 
 type IMItem = {
   id: ID
@@ -16,7 +17,7 @@ export class MItem<T extends IMItem = IMItem> {
   readonly hash: string
 
   constructor(args: PartialBy<T, 'hash'>) {
-    args['hash'] = args.hash ?? JSON.stringify(args)
+    args['hash'] = args.hash ?? MD5(args)
     return args as unknown as MItem<T>
   }
 }
