895abe7dd562073c27ed73e5ddc35dfe87365715
Trevor Sargent <trevor.a.sargent@gmail.com>
2023-07-15 13:57:16 -0700

    chore(cleanup): remove debug logs

--- libs/myko/nest/src/types.ts
+++ libs/myko/nest/src/types.ts
@@ -1,39 +1,6 @@
-import { Client, ID } from '@myko/core'
+import { ID } from '@myko/core'
 import { WSMCommandError } from '@myko/ws'
-import { Injectable } from '@nestjs/common'
 import { WsException } from '@nestjs/websockets'
-import * as WebSocket from 'ws'
-import { MykoEventBus } from './lib/busses'
-
-@Injectable()
-export class SocketRegistry extends Map<ID, Set<WebSocket>> {
-  constructor(private events: MykoEventBus) {
-    super()
-  }
-
-  register(id: ID, socket: WebSocket) {
-    if (!this.has(id)) {
-      this.set(id, new Set())
-    }
-
-    if (this.has(id) && this.get(id).has(socket)) {
-      return
-    }
-    socket.on('close', () => {
-      this.events.publishSet(
-        new Client({ connected: false, id }),
-        'client-disconnected',
-      )
-      this.get(id).delete(socket)
-    })
-    this.events.publishSet(
-      new Client({ id, connected: true }),
-      'client-connected',
-    )
-    this.get(id).add(socket)
-    console.log('registered', id, this.get(id).size)
-  }
-}
 
 export class CommandNotAuthorized
   extends WsException
