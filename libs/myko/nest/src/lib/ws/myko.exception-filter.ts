2709441eb42710c30b3a8363729458849a99119b
Trevor Sargent <trevor.a.sargent@gmail.com>
2023-07-17 14:58:36 -0700

    fix(myko): encode errors based on client protocol

--- libs/myko/nest/src/lib/ws/myko.exception-filter.ts
+++ libs/myko/nest/src/lib/ws/myko.exception-filter.ts
@@ -1,9 +1,19 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common'
 import { WsException } from '@nestjs/websockets'
 
+import { clientProtocols, encoders } from '../registry/client.protocols'
+import { MykoProtocol } from '@myko/core'
+
 @Catch(WsException)
 export class WsExceptionFilter implements ExceptionFilter {
+  constructor() {}
   catch(exception: any, host: ArgumentsHost) {
-    host.switchToWs().getClient().send(JSON.stringify(exception))
+    const client = host.switchToWs().getClient()
+
+    const protocol = clientProtocols.get(client)
+
+    const encoder = encoders.get(protocol ?? MykoProtocol.JSON)
+
+    client.send(encoder(exception))
   }
 }
