3c8339a4906c2cfccd9e242ce5db8535e108507a
Trevor Sargent <trevor.a.sargent@gmail.com>
2025-01-23 12:59:33 -0800

    feat(ui): settings page

--- libs/myko/ui/src/lib/index.ts
+++ libs/myko/ui/src/lib/index.ts
@@ -1,4 +1,4 @@
 // Reexport your entry components here
 export * from './services/client.js';
-export { default as Cluster } from './components/Cluster.svelte';
 export { default as Logs } from './components/Logs.svelte';
+export { default as ServerView } from './components/ServerView.svelte';
